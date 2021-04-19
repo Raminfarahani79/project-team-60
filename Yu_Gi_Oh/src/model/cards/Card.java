6389e9e3be5f07b29f0d042a746c5ad1bd9e7dd3
Ramin <Raminfarahani79@yahoo.com>
2021-04-19 21:41:45 +0430

    these are the variable of card class.

--- Yu_Gi_Oh/src/model/cards/Card.java
+++ Yu_Gi_Oh/src/model/cards/Card.java
@@ -1,4 +1,9 @@
 package model.cards;
 
 public class Card {
+    private String name;
+    private String description;
+    private boolean isHidden;
+    private Location location;
+    private Board board;
 }
