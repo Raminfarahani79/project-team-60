faf64c7bba2c6e347acae5daa501650ba33edad1
Ramin <Raminfarahani79@yahoo.com>
2021-04-19 22:08:20 +0430

    All trapcards need just name and description.

--- Yu_Gi_Oh/src/model/cards/trapcards/TrapCard.java
+++ Yu_Gi_Oh/src/model/cards/trapcards/TrapCard.java
@@ -1,4 +1,8 @@
 package model.cards.trapcards;
+import model.cards.Card;
 
-public class TrapCard {
+public abstract class TrapCard extends Card {
+    public TrapCard(String name, String description){
+        super(name,description);
+    }
 }
