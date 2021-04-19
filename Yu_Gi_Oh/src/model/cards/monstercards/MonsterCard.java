831b999ae2f066771fa3557cb9554d270df1ef5e
Ramin <Raminfarahani79@yahoo.com>
2021-04-19 22:16:23 +0430

    constructor of MonsterCard class

--- Yu_Gi_Oh/src/model/cards/monstercards/MonsterCard.java
+++ Yu_Gi_Oh/src/model/cards/monstercards/MonsterCard.java
@@ -9,5 +9,8 @@ public class MonsterCard extends Card {
     private boolean switchedPosition;
     private Position position = Position.DEFENCE;
 
+    public MonsterCard(String name, String description, String level, int attackPoint, int defencePoint){
+        super(name, description);
 
+    }
 }
