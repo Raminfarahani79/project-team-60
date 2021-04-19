27133fb7eecc1c9ea21f639a12b3e4d1dc421ef2
Ramin <Raminfarahani79@yahoo.com>
2021-04-19 22:13:36 +0430

    All the variable we need in monster class.

--- Yu_Gi_Oh/src/model/cards/monstercards/MonsterCard.java
+++ Yu_Gi_Oh/src/model/cards/monstercards/MonsterCard.java
@@ -1,7 +1,13 @@
 package model.cards.monstercards;
-
+import model.cards.Card;
 import model.cards.Position;
 
-public class MonsterCard {
+public class MonsterCard extends Card {
+    private int level;
+    private int defencePoint;
+    private int attackPoint;
+    private boolean switchedPosition;
+    private Position position = Position.DEFENCE;
+
 
 }
