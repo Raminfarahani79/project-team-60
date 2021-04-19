0e12d28d70a6bacd05ca662a1817b31608a7981b
Ramin <Raminfarahani79@yahoo.com>
2021-04-19 22:57:06 +0430

    getter and setter we need

--- Yu_Gi_Oh/src/model/cards/monstercards/MonsterCard.java
+++ Yu_Gi_Oh/src/model/cards/monstercards/MonsterCard.java
@@ -7,10 +7,66 @@ public class MonsterCard extends Card {
     private int defencePoint;
     private int attackPoint;
     private boolean switchedPosition;
+    private boolean attacked;
     private Position position = Position.DEFENCE;
 
     public MonsterCard(String name, String description, String level, int attackPoint, int defencePoint){
         super(name, description);
+        setLevel(level);
+        setAttackPoint(attackPoint);
+        setDefencePoint(defencePoint);
+        setAttacked(false);
+        setSwitchedPosition(false);
+    }
 
+    public void setAttacked(boolean attacked) {
+        this.attacked = attacked;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void setDefencePoint(int defencePoint) {
+        this.defencePoint = defencePoint;
+    }
+
+    public void setAttackPoint(int attackPoint) {
+        this.attackPoint = attackPoint;
+    }
+
+    public void setSwitchedPosition(boolean switchedPosition) {
+        this.switchedPosition = switchedPosition;
+    }
+
+    public boolean isAttacked() {
+        return attacked;
+    }
+
+    public void setHidden(boolean hidden){
+        super.setHidden(hidden);
+    }
+
+    public void setPosition(Position position){
+        this.position = position;
+    }
+    public int getLevel() {
+        return level;
+    }
+
+    public int getDefencePoint() {
+        return defencePoint;
+    }
+
+    public int getAttackPoint() {
+        return attackPoint;
+    }
+
+    public boolean isSwitchedPosition() {
+        return switchedPosition;
+    }
+
+    public Position getPosition() {
+        return position;
     }
 }
