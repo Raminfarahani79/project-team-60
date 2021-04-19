33359e92555bb33bf802201df07db335adb71cfe
Ramin <Raminfarahani79@yahoo.com>
2021-04-19 21:44:56 +0430

    first constructor was made because some card just have name and description such spell and trap card

--- Yu_Gi_Oh/src/model/cards/Card.java
+++ Yu_Gi_Oh/src/model/cards/Card.java
@@ -1,9 +1,14 @@
 package model.cards;
 
-public class Card {
+public abstract class Card {
     private String name;
     private String description;
     private boolean isHidden;
     private Location location;
     private Board board;
+
+    public Card(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
 }
