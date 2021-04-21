13436e973ee39776db7ae3ba8472f56cb6ecda64
Ramin <Raminfarahani79@yahoo.com>
2021-04-21 09:29:19 +0430

    update each spellcard with its action class

--- Yu_Gi_Oh/src/model/cards/spellcards/MonsterReborn.java
+++ Yu_Gi_Oh/src/model/cards/spellcards/MonsterReborn.java
@@ -1,4 +1,14 @@
 package model.cards.spellcards;
 
+import model.cards.monstercards.MonsterCard;
+
 public class MonsterReborn extends SpellCard {
+
+    public MonsterReborn(String name, String description) {
+        super(name, description);
+    }
+
+    public void action(MonsterCard monster){
+
+    }
 }
