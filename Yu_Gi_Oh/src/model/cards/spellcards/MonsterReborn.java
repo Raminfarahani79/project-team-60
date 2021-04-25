8ba222c51c21a7c34f8ed7a58f4d70115b2bcd69
Ramin <Raminfarahani79@yahoo.com>
2021-04-25 16:12:18 +0430

    update trap cards.

--- Yu_Gi_Oh/src/model/cards/spellcards/MonsterReborn.java
+++ Yu_Gi_Oh/src/model/cards/spellcards/MonsterReborn.java
@@ -6,6 +6,7 @@ public class MonsterReborn extends SpellCard {
 
     public MonsterReborn(String name, String description) {
         super(name, description);
+
     }
 
     public void action(MonsterCard monster){
