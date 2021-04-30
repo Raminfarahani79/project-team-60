580aa80369fabdbd6010a4ead71a5bbb0edd2696
hana-chg <75851913+hana-chg@users.noreply.github.com>
2021-04-30 11:11:16 +0430

    added methods to UserDecks class

--- Yu_Gi_Oh/src/model/UserDecks.java
+++ Yu_Gi_Oh/src/model/UserDecks.java
@@ -3,6 +3,66 @@ package model;
 import java.util.ArrayList;
 
 public class UserDecks {
-    public ArrayList<Deck> allDecks;
+    private ArrayList<Deck> allDecks;
+    private Deck activeDeck;
 
+    public UserDecks() {
+        allDecks = new ArrayList<>();
+    }
+
+    public void createDeck(String deckName) {
+        allDecks.add(new Deck(deckName));
+    }
+
+    public boolean isDeckNameUsedBefore(String deckName) {
+        if (allDecks.size() > 0) {
+            for (Deck deck : allDecks) {
+                if (deck.getName().matches(deckName)) return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    public void deleteDeck(String deckName) {
+        allDecks.remove(this.getDeckByName(deckName));
+    }
+
+    public void setActiveDeck(String deckName) {
+        activeDeck = getDeckByName(deckName);
+    }
+
+
+    public String showAllDecks() {
+        StringBuilder allDecksStringBuilder = new StringBuilder();
+        allDecksStringBuilder.append("Decks:\nActive deck:\n");
+        if (activeDeck != null) {
+            allDecksStringBuilder.append(activeDeck.getName() +
+                    ": main deck " + activeDeck.getMainDeck().allCards.size() +
+                    ",side deck " + activeDeck.getSideDeck().allCards.size() +
+                    ", " + activeDeck.getValidity()+"\n");
+        }
+        allDecksStringBuilder.append("Other decks:");
+        //todo sort by alphabetical order
+        if (allDecks.size() > 0) {
+            for (Deck deck : allDecks) {
+                if(activeDeck !=null){
+                    if (deck.getName().matches(activeDeck.getName())) continue;
+                }
+                allDecksStringBuilder.append("\n"+deck.getName() +
+                        ": main deck " + deck.getMainDeck().allCards.size() +
+                        ",side deck " + deck.getSideDeck().allCards.size() +
+                        ", " + deck.getValidity());
+            }
+        }
+        return allDecksStringBuilder.toString();
+    }
+
+
+    public Deck getDeckByName(String deckName) {
+        for (Deck deck : allDecks) {
+            if (deck.getName().matches(deckName)) return deck;
+        }
+        return null;
+    }
 }
