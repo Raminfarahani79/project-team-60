806e7041ce70be9a089e19dc1b3ff740e5fefb40
hana <sam@example.com>
2021-05-03 18:28:49 +0430

    debugged

--- Yu_Gi_Oh/src/main/java/view/menus/DuelMenu.java
+++ Yu_Gi_Oh/src/main/java/view/menus/DuelMenu.java
@@ -2,13 +2,18 @@ package view.menus;
 
 import controller.DuelMenuController;
 
+
+
 public class DuelMenu extends Menu {
+    private DuelMenuController duelMenuController;
     public DuelMenu(Menu parentMenu) {
         super("Duel", parentMenu);
+        duelMenuController = new DuelMenuController();
     }
 
     @Override
     public void run() {
+        duelMenuController.processCommand(input);
         runMenuCommands();
     }
 }
