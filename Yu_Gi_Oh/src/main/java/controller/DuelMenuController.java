578df90d3550d4a1c982d36d87a14c047e5fe9bb
hana <sam@example.com>
2021-05-03 18:27:43 +0430

    added  command processor for this controller

--- Yu_Gi_Oh/src/main/java/controller/DuelMenuController.java
+++ Yu_Gi_Oh/src/main/java/controller/DuelMenuController.java
@@ -1,5 +1,21 @@
 package controller;
 
-public class DuelMenuController {
+import model.Phases;
+import view.menus.Menu;
 
+public class DuelMenuController {
+    private Phases phase = Phases.DRAW;
+
+    public void processCommand(String command) {
+        if (command.matches("select -d")) deselect(command);
+        else if (command.matches("select \\S+")) selectCard(command);
+        else if (command.matches("next phase")) nextPhase();
+    }
+
+    private void deselect(String command) {}
+    private void selectCard(String command) {}
+    private void nextPhase() {
+        phase = phase.next();
+        Menu.print("phase: "+phase.toString());
+    }
 }
