6ef1f29dddf3fe4533798bc4a441d99d7a7cb39f
hana-chg <75851913+hana-chg@users.noreply.github.com>
2021-05-04 08:10:55 +0430

    added clone method

--- Yu_Gi_Oh/src/main/java/model/SideDeck.java
+++ Yu_Gi_Oh/src/main/java/model/SideDeck.java
@@ -19,6 +19,15 @@ public class SideDeck  {
     public void removeCardFromDeck(String cardName) {
 
     }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        SideDeck sideDeck = new SideDeck();
+        sideDeck.allCards = (ArrayList) this.allCards.clone();
+        sideDeck.isFull = this.isFull;
+        return sideDeck;
+    }
+
      public Boolean doesDeckHaveThisCard(String cardName) {
         if (allCards.size() > 0) {
             for (Card card : allCards) {
