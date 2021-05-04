c2b34b92fea85b4051cbb7e861b03933c8210d91
Ghazal Darabi <gh.darabi80@gmail.com>
2021-05-04 18:02:43 +0430

    ProfileMenu methods added.

--- Yu_Gi_Oh/src/main/java/view/menus/ProfileMenu.java
+++ Yu_Gi_Oh/src/main/java/view/menus/ProfileMenu.java
@@ -1,5 +1,9 @@
 package view.menus;
 
+import model.User;
+
+import java.util.regex.Matcher;
+
 public class ProfileMenu extends Menu {
     public ProfileMenu(Menu parentMenu) {
         super("Profile", parentMenu);
@@ -9,4 +13,35 @@ public class ProfileMenu extends Menu {
     public void run() {
         runMenuCommands();
     }
+
+    private void changeNickname(String command) {
+        Matcher nicknameMatcher = getMatcher("--nickname (\\S+)", command);
+        if (nicknameMatcher.find()) {
+            String nickname = nicknameMatcher.group(1);
+            for (User user : User.getAllUsers()) {
+                if (user.getNickname().equals(nickname)) {
+                    print("user with nickname " + nickname + " already exists");
+                    return;
+                }
+            }
+            User.currentUser.setNickname(nickname);
+            print("nickname changed successfully!");
+        }
+    }
+
+    private void changePassword(String command) {
+        Matcher currentPasswordMatcher = getMatcher("--current (\\S+)", command);
+        Matcher newPasswordMatcher = getMatcher("--new (\\S+)", command);
+        if (currentPasswordMatcher.find() && newPasswordMatcher.find()) {
+            String currentPassword = currentPasswordMatcher.group(1);
+            String newPassword = newPasswordMatcher.group(1);
+            if (!User.currentUser.getPassword().equals(currentPassword)) print("current password is invalid");
+            else if (User.currentUser.getPassword().equals(newPassword)) print("￼please enter a new password");
+            else {
+                User.currentUser.setPassword(newPassword);
+                print("password changed successfully!");
+            }
+        }
+    }
+
 }
