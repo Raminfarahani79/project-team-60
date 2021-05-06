e6a17a339e6afbfd16adb8fb73c0ab5c9c49b9f5
Ghazal Darabi <gh.darabi80@gmail.com>
2021-05-06 14:54:44 +0430

    small change in Controller.

--- Yu_Gi_Oh/src/main/java/controller/Controller.java
+++ Yu_Gi_Oh/src/main/java/controller/Controller.java
@@ -4,6 +4,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public abstract class Controller {
+    protected abstract void processCommand(String command);
+
     public Matcher getMatcher(String regex, String command) {
         Pattern pattern = Pattern.compile(regex);
         return pattern.matcher(command);
