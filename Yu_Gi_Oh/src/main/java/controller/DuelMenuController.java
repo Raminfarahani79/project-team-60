abca60fed01242658e65e2f753671c2ba824aa0c
hana-chg <75851913+hana-chg@users.noreply.github.com>
2021-05-15 15:43:54 +0430

    added select method to DuelMenuController

--- Yu_Gi_Oh/src/main/java/controller/DuelMenuController.java
+++ Yu_Gi_Oh/src/main/java/controller/DuelMenuController.java
@@ -1,21 +1,64 @@
 package controller;
 
-import model.Phases;
-import view.menus.Menu;
+import model.game.Game;
 
-public class DuelMenuController {
-    private Phases phase = Phases.DRAW;
+public class DuelMenuController extends Controller {
+    private static DuelMenuController instance;
+    public Game game;
+
+    private DuelMenuController() {
+    }
+
+
+    public static DuelMenuController getInstance() {
+        return instance;
+    }
+
 
     public void processCommand(String command) {
-        if (command.matches("select -d")) deselect(command);
-        else if (command.matches("select \\S+")) selectCard(command);
-        else if (command.matches("next phase")) nextPhase();
+        if (command.matches("select -d")) deselectCard(command);
+        else if (command.matches("select ((--spell \\d)|(--deck)|(--monster \\d)|(--field)|(--hand))"))
+            selectFromOwnerCards(command.replace("select ", ""));
+        else if (command.matches("select (?=.*(((--spell \\d)|(--deck)|(--monster \\d)|(field)) \\d))(?=.*(--opponent))((\\1 \\2)|(\\2 \\1))")) {
+            command = command.replace("--opponent", "");
+            selectFromOpponentCards(command.replace("select ", ""));
+        } else if (command.matches("next phase")) nextPhase();
     }
 
-    private void deselect(String command) {}
-    private void selectCard(String command) {}
-    private void nextPhase() {
-        phase = phase.next();
-        Menu.print("phase: "+phase.toString());
+    public void setGame(Game game) {
+        this.game = game;
     }
+
+    private void deselectCard(String command) {
+        Game.getCurrentGame().setSelectedCard(null);
+    }
+
+    private void selectFromOpponentCards(String location) {
+        if (game.getOpponentPlayer().getBoard().getCardInLocation(location) == null)
+            print("no card found in the given position");
+        else
+            game.setSelectedCard(game.getOpponentPlayer().getBoard().getCardInLocation(location));
+    }
+
+    private void selectFromOwnerCards(String location) {
+        if (game.getCurrentPlayer().getBoard().getCardInLocation(location) == null)
+            print("no card found in the given position");
+        else
+            game.setSelectedCard(game.getCurrentPlayer().getBoard().getCardInLocation(location));
+    }
+
+    private void nextPhase() {
+        Game.getCurrentGame().nextPhase();
+        print("phase: " + Game.getCurrentGame().getPhase().toString());
+    }
+
+//    public void showGameBoard() {
+//        StringBuilder board = new StringBuilder();
+//        board.append(game.getCurrentPlayer().getBoard().show());
+//        if (game.getCurrentPlayer() == game.getOpponentPlayer())
+//            board.append(game.getUserPlayer().getBoard().show());
+//        else
+//            board.append(game.getOpponentPlayer().getBoard().show());
+//        print(board.toString());
+//    }
 }
