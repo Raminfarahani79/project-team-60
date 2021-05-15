ecb668d0d81933007c9b37f20a4a597337c36916
hana-chg <75851913+hana-chg@users.noreply.github.com>
2021-05-16 01:39:17 +0430

    added showGameBoard method and few small changes

--- Yu_Gi_Oh/src/main/java/controller/Controller.java
+++ Yu_Gi_Oh/src/main/java/controller/Controller.java
@@ -2,11 +2,12 @@ package controller;
 
 import view.menus.Menu;
 
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public abstract class Controller {
-    protected abstract void processCommand(String command);
+    protected abstract void processCommand(String command) throws IOException;
 
     public Matcher getMatcher(String regex, String command) {
         Pattern pattern = Pattern.compile(regex);
--- Yu_Gi_Oh/src/main/java/controller/DuelMenuController.java
+++ Yu_Gi_Oh/src/main/java/controller/DuelMenuController.java
@@ -1,5 +1,6 @@
 package controller;
 
+
 import model.game.Game;
 
 public class DuelMenuController extends Controller {
@@ -17,9 +18,9 @@ public class DuelMenuController extends Controller {
 
     public void processCommand(String command) {
         if (command.matches("select -d")) deselectCard(command);
-        else if (command.matches("select ((--spell \\d)|(--deck)|(--monster \\d)|(--field)|(--hand))"))
+        else if (command.matches("select ((--\\S+ \\d)|(--\\S+))"))
             selectFromOwnerCards(command.replace("select ", ""));
-        else if (command.matches("select (?=.*(((--spell \\d)|(--deck)|(--monster \\d)|(field)) \\d))(?=.*(--opponent))((\\1 \\2)|(\\2 \\1))")) {
+        else if (command.matches("select (?=.*(--\\b(?!opponent\\b)\\w+))(?=.*(--opponent))((\\1 \\2)|(\\2 \\1))")) {
             command = command.replace("--opponent", "");
             selectFromOpponentCards(command.replace("select ", ""));
         } else if (command.matches("next phase")) nextPhase();
@@ -30,10 +31,15 @@ public class DuelMenuController extends Controller {
     }
 
     private void deselectCard(String command) {
-        Game.getCurrentGame().setSelectedCard(null);
+        if (game.getSelectedCard() == null)
+            print("no card is selected yet");
+        else
+            game.setSelectedCard(null);
     }
 
     private void selectFromOpponentCards(String location) {
+        if (!location.matches("--((monster \\d{1,5})|(spell \\d{1,5})|(field)|(hand \\d{1,6}))"))
+            print("invalid selection");
         if (game.getOpponentPlayer().getBoard().getCardInLocation(location) == null)
             print("no card found in the given position");
         else
@@ -41,6 +47,8 @@ public class DuelMenuController extends Controller {
     }
 
     private void selectFromOwnerCards(String location) {
+        if (!location.matches("--((monster \\d{1,5})|(spell \\d{1,5})|(field)|(hand \\d{1,6}))"))
+            print("invalid selection");
         if (game.getCurrentPlayer().getBoard().getCardInLocation(location) == null)
             print("no card found in the given position");
         else
@@ -48,17 +56,42 @@ public class DuelMenuController extends Controller {
     }
 
     private void nextPhase() {
-        Game.getCurrentGame().nextPhase();
-        print("phase: " + Game.getCurrentGame().getPhase().toString());
+        game.nextPhase();
+        print("phase: " + game.getPhase().toString());
+        switch (game.getPhase()) {
+            case DRAW:
+                draw();
+            case MAIN_1:
+                showGameBoard();
+            case END:
+                print("its <next player nickname>’s turn");
+        }
     }
 
-//    public void showGameBoard() {
-//        StringBuilder board = new StringBuilder();
-//        board.append(game.getCurrentPlayer().getBoard().show());
-//        if (game.getCurrentPlayer() == game.getOpponentPlayer())
-//            board.append(game.getUserPlayer().getBoard().show());
-//        else
-//            board.append(game.getOpponentPlayer().getBoard().show());
-//        print(board.toString());
-//    }
+    private void draw() {
+        game.getCurrentPlayer().getBoard().addCardFromDeckToHand();
+        print("new card added to the hand : "+
+                game.getCurrentPlayer().getBoard().getHand().get(game.getCurrentPlayer().getBoard().getHand().size()-1));
+    }
+
+    private void summon () {
+        CardNotSelectHandler cardNotSelectHandler = new CardNotSelectHandler();
+        CardCantBeSummonedHandler cardCantBeSummonedHandler = new CardCantBeSummonedHandler();
+        NotAllowedInCurrentPhase notAllowedInCurrentPhase = new NotAllowedInCurrentPhase();
+        FullMonsterZoneHandler fullMonsterZoneHandler = new FullMonsterZoneHandler();
+        CardAlreadySetOrSummoned cardAlreadySetOrSummoned = new CardAlreadySetOrSummoned();
+        cardAlreadySetOrSummoned.setNextHandler(cardNotSelectHandler);
+        cardCantBeSummonedHandler.setNextHandler(notAllowedInCurrentPhase);
+        notAllowedInCurrentPhase.setNextHandler(fullMonsterZoneHandler);
+        fullMonsterZoneHandler.setNextHandler(cardNotSelectHandler);
+        cardAlreadySetOrSummoned.setNextHandler(null);
+
+    }
+
+    public void showGameBoard() {
+        StringBuilder board = new StringBuilder();
+        board.append(game.getOpponentPlayer().showBoard()).reverse();
+        board.append(game.getCurrentPlayer().showBoard());
+        print(board.toString());
+    }
 }
