5ae23e87cd943b14a7e39b8809aceac169f290cb
hana-chg <75851913+hana-chg@users.noreply.github.com>
2021-05-19 18:55:29 +0430

    divided toString method into two conditions

--- Yu_Gi_Oh/src/main/java/model/game/Board.java
+++ Yu_Gi_Oh/src/main/java/model/game/Board.java
@@ -1,6 +1,7 @@
 package model.game;
 
 import model.Deck;
+import model.User;
 import model.cards.Card;
 import model.cards.Location;
 import model.cards.monstercards.MonsterCard;
@@ -50,22 +51,27 @@ public class Board {
 
     public void putCardInMonsterZone(Card card) {
         card.setLocation(Location.MONSTER_ZONE);
-        for (MonsterCard cardInZone : monsterZone) {
-            if (cardInZone == null)
-                cardInZone = (MonsterCard) card;
+        for (int i = 0; i < monsterZone.length; i++) {
+         if (monsterZone[i] == null) {
+             monsterZone[i] = (MonsterCard) card;
+             break;
+         }
         }
     }
 
     public void putCardInSpellAndTrapZone(Card card) {
         card.setLocation(Location.SPELL_TRAP_ZONE);
-        for (Card cardINZone : spellAndTrapZone) {
-            if (cardINZone == null)
-                cardINZone = card;
+        for (int i = 0; i < spellAndTrapZone.length; i++) {
+            if (spellAndTrapZone[i] == null) {
+                spellAndTrapZone[i] = card;
+                break;
+            }
         }
     }
 
     public void putCardInHand(Card card) {
         card.setLocation(Location.HAND);
+        hand.add(card);
     }
 
     public ArrayList<Card> getGraveyard() {
@@ -97,17 +103,13 @@ public class Board {
 
     public void removeCardFromMonsterZone(Card card) {
         card.setLocation(null);
-
-    }
+        for (int i = 0; i < monsterZone.length; i++) {
+            if (monsterZone[i] == card) monsterZone[i] = null;
+        }    }
 
     public void removeCardFromHand(Card card) {
         card.setLocation(null);
-        for (int i = 0; i < 5; i++) {
-            if (monsterZone[i] == card) {
-                monsterZone[i] = null;
-                break;
-            }
-        }
+        hand.remove(card);
     }
 
     public void removeCardFromFieldZone(Card card) {
@@ -127,20 +129,21 @@ public class Board {
 
     public void removeCardFromMainDeck(Card card) {
         card.setLocation(null);
-       deck.getMainDeck().getAllCards().remove(card);
+        deck.getMainDeck().getAllCards().remove(card);
     }
 
     public Card getCardInLocation(String location) {
         if (location.matches("--monster \\d")) {
             int monsterNumber = Integer.parseInt(location.replace("--monster ", ""));
-            return monsterZone[monsterNumber];
+            return monsterZone[monsterNumber - 1];
         } else if (location.matches("--spell \\d")) {
             int spellNumber = Integer.parseInt(location.replace("--spell ", ""));
-            return spellAndTrapZone[spellNumber];
+            return spellAndTrapZone[spellNumber - 1];
         } else if (location.matches("--hand \\d")) {
-            int handNumber = Integer.parseInt(location.replace("--spell ", ""));
-            return hand.get(handNumber);
-        } else if (location.matches("field"))
+            int handNumber = Integer.parseInt(location.replace("--hand ", ""));
+            if (handNumber > hand.size()) return null;
+            return hand.get(handNumber - 1);
+        } else if (location.matches("--field"))
             return fieldZone;
         return null;
     }
@@ -154,35 +157,71 @@ public class Board {
             Game.getCurrentGame().endGame(Game.getCurrentGame().getOpponentPlayer());
     }
 
-    public String toString() {
+    public String toStringAsCurrent() {
+        Player current = Game.getCurrentGame().getCurrentPlayer();
         StringBuilder returnString = new StringBuilder();
-        if (fieldZone == null) returnString.append("E\t\t\t\t\t\t");
-        else returnString.append("O\t\t\t\t\t\t");
+        if (fieldZone == null) returnString.append("E\t  \t  \t  \t  \t  \t");
+        else returnString.append("O\t  \t  \t  \t  \t  \t");
         returnString.append(graveyard.size() + "\n\t");
         int[] counter = {5, 3, 1, 2, 4};
         for (int i : counter) {
-            if (monsterZone[i] == null) returnString.append("E \t");
+            if (monsterZone[i - 1] == null) returnString.append("E \t");
             else {
-                switch (monsterZone[i].getPosition()) {
+                switch (monsterZone[i - 1].getPosition()) {
                     case ATTACK:
                         returnString.append("OO\t");
+                        break;
                     case DEFENCE:
-                        if (monsterZone[i].isHidden()) returnString.append("DH");
-                        else returnString.append("DO");
+                        if (monsterZone[i - 1].isHidden()) returnString.append("DH\t");
+                        else returnString.append("DO\t");
+                        break;
                 }
             }
-            returnString.append("\n");
         }
+        returnString.append("\n\t");
         for (int i : counter) {
-            if (spellAndTrapZone[i] == null) returnString.append("E \t");
-            else if (spellAndTrapZone[i].isHidden()) returnString.append("H \t");
-            else returnString.append("O\t");
+            if (spellAndTrapZone[i - 1] == null) returnString.append("E \t");
+            else if (spellAndTrapZone[i - 1].isHidden()) returnString.append("H \t");
+            else returnString.append("O \t");
         }
-        returnString.append("\n");
+        returnString.append("\n  \t  \t  \t  \t  \t  \t" + deck.getMainDeck().getAllCards().size() + "\n");
         for (int i = 1; i <= hand.size(); i++) {
             returnString.append("c\t");
         }
+        returnString.append("\n" + current.getUsername() + ":" + current.getLifePoint());
         return returnString.toString();
     }
 
+    public String toStringAsOpponent() {
+        Player opponent = Game.getCurrentGame().getOpponentPlayer();
+        StringBuilder returnString = new StringBuilder();
+        returnString.append(opponent.getUsername() + ":" + opponent.getLifePoint() + "\n\t");
+        for (int i = 0; i < hand.size(); i++) returnString.append("c \t");
+        returnString.append("\n" + opponent.getBoard().deck.getMainDeck().getAllCards().size() + "\n\t");
+        int[] counter = {4, 2, 1, 3, 5};
+        for (int i : counter) {
+            if (spellAndTrapZone[i - 1] == null) returnString.append("E \t");
+            else if (spellAndTrapZone[i - 1].isHidden()) returnString.append("H \t");
+            else returnString.append("O \t");
+        }
+        returnString.append("\n\t");
+        for (int i : counter) {
+            if (monsterZone[i - 1] == null) returnString.append("E \t");
+            else {
+                switch (monsterZone[i - 1].getPosition()) {
+                    case ATTACK:
+                        returnString.append("OO\t");
+                        break;
+                    case DEFENCE:
+                        if (monsterZone[i - 1].isHidden()) returnString.append("DH\t");
+                        else returnString.append("DO\t");
+                        break;
+                }
+            }
+        }
+        returnString.append("\n" + opponent.getBoard().getGraveyard().size() + "\t  \t  \t  \t  \t  \t");
+        if (opponent.getBoard().getCardInLocation("--field") == null) returnString.append("E ");
+        else returnString.append("O ");
+        return returnString.toString();
+    }
 }
