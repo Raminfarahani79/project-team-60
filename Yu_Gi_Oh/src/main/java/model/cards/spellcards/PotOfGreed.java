5456b3fbfe9692a3cd351531e1ac0024fa159df7
Ramin <Raminfarahani79@yahoo.com>
2021-05-19 17:26:11 +0430

    update Raigeki action class

--- Yu_Gi_Oh/src/main/java/model/cards/spellcards/PotOfGreed.java
+++ Yu_Gi_Oh/src/main/java/model/cards/spellcards/PotOfGreed.java
@@ -1,5 +1,6 @@
 package model.cards.spellcards;
 
+import model.User;
 import model.cards.monstercards.MonsterCard;
 
 public class PotOfGreed extends SpellCard{
@@ -11,7 +12,9 @@ public class PotOfGreed extends SpellCard{
     }
 
     public void action(MonsterCard monster){
-
+        User user = User.currentUser;
+        user.getBoard().addCardFromDeckToHand();
+        user.getBoard().addCardFromDeckToHand();
     }
 
     @Override
